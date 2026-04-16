444152f39b416aac8549c6c6be0b33e6b9835605
olakunle0044 <olakunle0044@gmail.com>
2026-04-16 11:52:49 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+/* SLIDER */
+const grid = document.getElementById("grid");
+
+if (grid) {
+  let index = 0;
+  const visible = 3;
+  const total = grid.children.length;
+  const imageWidth = 300;
+
+  function updateSlider() {
+    grid.style.transform = `translateX(-${index * imageWidth}px)`;
+  }
+
+  function nextSlide() {
+    index++;
+
+    if (index > total - visible) {
+      index = 0;
+    }
+
+    updateSlider();
+  }
+
+  function prevSlide() {
+    index--;
+
+    if (index < 0) {
+      index = total - visible;
+    }
+
+    updateSlider();
+  }
+
+  setInterval(nextSlide, 3000);
+}
+
+
+/* NAV MENU */
+const hamMenu = document.querySelector(".ham-menu");
+const navMenu = document.querySelector(".nav-menu");
+
+if (hamMenu && navMenu) {
+  hamMenu.addEventListener("click", () => {
+    hamMenu.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
+
+  document.querySelectorAll(".nav-link").forEach(n =>
+    n.addEventListener("click", () => {
+      hamMenu.classList.remove("active");
+      navMenu.classList.remove("active");
+    })
+  );
+}
